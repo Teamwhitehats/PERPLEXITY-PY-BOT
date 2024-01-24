7b1ca8833b684384cf4e21333526e9878a7b0c99
PRAPTI220098 <140289767+PRAPTI220098@users.noreply.github.com>
2024-01-24 20:55:57 +0530

    Update hh.py

--- hh.py
+++ hh.py
@@ -1,17 +1,16 @@
 import flask
-from threading import Thread
+import threading
 
-app = flask.Flask('')
-
+app = flask.Flask(__name__)
 
 @app.route('/')
 def home():
-  return "<b> hello</b>"
+    return "<b>hello</b>"
 
 def run():
-  app.run(host='0.0.0.0', port=8080)
-
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+    t = threading.Thread(target=run)
+    t.start()
+
